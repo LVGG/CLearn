8f903040c41858240092d3e10610cfa21d65d96e
beaster <beaster@yeah.net>
2025-07-24 20:28:44 +0800

    v0.01

---
+++ basic/extern/A.c
@@ -0,0 +1,14 @@
+//
+// Created by mac on 2025/7/24.
+//
+
+#include <stdio.h>
+#include "A.h"
+
+int main(int argc, char const *argv[])
+{
+    int a = 10086;
+    printf("%d\n", a);
+    return 0;
+}
+
---
+++ basic/extern/A.h
@@ -0,0 +1,10 @@
+//
+// Created by mac on 2025/7/24.
+//
+
+#ifndef A_H
+#define A_H
+
+// extern int a;
+
+#endif //A_H
